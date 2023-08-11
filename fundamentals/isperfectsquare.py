c20119474340c7c3005f8cb5602bfb20050ab058
l4t8 <105388681+l4t8@users.noreply.github.com>
2023-08-11 10:33:19 +0200

    Add files via upload

---
+++ fundamentals/isperfectsquare.py
@@ -0,0 +1,7 @@
+from math import sqrt,ceil,fabs,floor
+def isPP(n):
+    for i in range(2,ceil(sqrt(n))+1):
+        x = pow(n,1/i)
+        if ceil(x)-x < 0.00000001:x = ceil(x)
+        elif x-floor(x) < 0.00000001:x = floor(x)
+        if int(x) == float(x): return [int(x),i]
